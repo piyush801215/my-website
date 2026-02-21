dce70d11505a87ddafee61f95d8d779e3695dc47
Piyush801215 <sharmapromit369@gmail.com>
2026-02-22 01:34:23 +0530

    fix run.py remove blocking code

--- remo d souz/run.py
+++ remo d souz/run.py
@@ -9,18 +9,13 @@ with app.app_context():
     db.create_all()
 
 def setup_initial_admin():
-    with app.app_context():
-        db.create_all()
-        if not User.query.filter_by(role='super_admin').first():
+            if not User.query.filter_by(role='super_admin').first():
             print("Creating initial Super Admin...")
             u = User(username=os.getenv('ADMIN_USER'), role='super_admin')
             u.set_password(os.getenv('ADMIN_PASS'))
             db.session.add(u)
             db.session.commit()
             print("Super Admin Created.")
-
-if __name__ == "__main__":
-    setup_initial_admin()
-    if app.config.get('BOT_TOKEN'):
-        start_telegram_bot(app)
-    app.run(host="0.0.0.0", port=5000)
+@app.route("/")
+def home():
+    return "Server is working 🚀"
