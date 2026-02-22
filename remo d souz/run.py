9d6978482c56e0bbbdee221863eb1266a62e3d82
Piyush801215 <sharmapromit369@gmail.com>
2026-02-22 12:01:30 +0530

    fix admin creation

--- remo d souz/run.py
+++ remo d souz/run.py
@@ -9,13 +9,18 @@ with app.app_context():
     db.create_all()
 
 def setup_initial_admin():
-            if not User.query.filter_by(role='super_admin').first():
+    with app.app_context():
+        if not User.query.filter_by(role='super_admin').first():
             print("Creating initial Super Admin...")
             u = User(username=os.getenv('ADMIN_USER'), role='super_admin')
             u.set_password(os.getenv('ADMIN_PASS'))
             db.session.add(u)
             db.session.commit()
             print("Super Admin Created.")
+
+# ✅ CALL AFTER DEFINITION
+setup_initial_admin()
+
 @app.route("/")
 def home():
     return "Server is working 🚀"
